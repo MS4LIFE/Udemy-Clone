e7c67369cd62487e307c0f8233d232a3f1c944d8
unknown <mo7ammed.sabre@gmail.com>
2023-07-31 08:24:36 +0300

    31-7-pull

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-document.getElementById("2").style.display="none"
-document.getElementById("3").style.display="none"
+document.getElementById("2").style.display = "none"
+document.getElementById("3").style.display = "none"
 document.getElementsByClassName("1")[0].classList.add("underLine")
-function tabs(targt){
+function tabs(targt) {
     console.log(targt.classList[1]);
     document.getElementsByClassName("1")[0].classList.remove("underLine")
     document.getElementsByClassName("2")[0].classList.remove("underLine")
     document.getElementsByClassName("3")[0].classList.remove("underLine")
 
     targt.classList.add("underLine")
-    document.getElementById("1").style.display="none"
-    document.getElementById("2").style.display="none"
-    document.getElementById("3").style.display="none"
-    if(targt.classList[1]==1){
-        document.getElementById("1").style.display="block"
-    }else if(targt.classList[1]==2){
-        document.getElementById("2").style.display="block"
-    }else if(targt.classList[1]==3){
-        document.getElementById("3").style.display="block"
+    document.getElementById("1").style.display = "none"
+    document.getElementById("2").style.display = "none"
+    document.getElementById("3").style.display = "none"
+    if (targt.classList[1] == 1) {
+        document.getElementById("1").style.display = "block"
+    } else if (targt.classList[1] == 2) {
+        document.getElementById("2").style.display = "block"
+    } else if (targt.classList[1] == 3) {
+        document.getElementById("3").style.display = "block"
     }
 
 }
